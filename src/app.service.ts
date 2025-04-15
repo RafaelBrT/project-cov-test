6e3306670d9310b985f550a482abd78e7c172b2f
Rafael Bueno <rafaelbuenodelarabrt@gmail.com>
2025-04-14 23:13:43 -0300

    teste coverage pr

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,6 +4,9 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 export class AppService {
   getHello(teste?: string): string {
     if (teste === 'que isso') new NotFoundException('opa')
+    if (teste === 'que isso2') new NotFoundException('opa2')
+    if (teste === 'que isso3') new NotFoundException('opa3')
+
     return teste || 'Hello World!';
   }
 }
